395372861137c649ce7e7f8034dfbc139e55a9dd
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-09-09 15:48:22 +0300

    Add admin's panel in profile

--- csv_srv/urls.py
+++ csv_srv/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('csvfiles/', views.CsvFileList.as_view(), name='csvfile-list'),
+    path('', views.CsvUploadView.as_view(), name='csv-upload'),
 ]
--- csv_srv/views.py
+++ csv_srv/views.py
@@ -1,7 +1,30 @@
 from rest_framework import generics
 from .models import CsvFile
 from .serializers import CsvFileSerializer
+from django.shortcuts import render
+from django.views import View
+from .forms import CsvUploadForm
 
 class CsvFileList(generics.ListCreateAPIView):
     queryset = CsvFile.objects.all()
     serializer_class = CsvFileSerializer
+
+class CsvUploadView(View):
+    template_name = 'csv_srv/csv_upload.html'
+
+    def get(self, request):
+        form = CsvUploadForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = CsvUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Обрабатывайте файл здесь, например, сохраняйте его в базу данных или выполняйте другие действия
+            uploaded_file = form.cleaned_data['csv_file']
+            # Ваша логика обработки файла
+
+            # После успешной обработки, вы можете перенаправить пользователя или отобразить сообщение об успехе
+            return render(request, self.template_name, {'form': form, 'success_message': 'File uploaded successfully!'})
+
+        # Если форма не валидна, отобразите ошибки
+        return render(request, self.template_name, {'form': form})
